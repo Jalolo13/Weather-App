3dcdfe69bd27443682cbe34c9ea921c6d3a99ada
Jalolo13 <jalololo13@gmail.com>
2022-04-14 13:26:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const inputval = document.querySelector("#cityinput"),
+    display = document.querySelector(".display"),
+    btn = document.querySelector("#add"),
+    city = document.querySelector("#cityoutput"),
+    descrip = document.querySelector("#description"),
+    temp = document.querySelector("#temp"),
+    wind = document.querySelector("#wind"),
+    time = document.querySelector("#time"),
+    apik = "3045dd712ffe6e702e3245525ac7fa38";
+
+const currentYear = new Date().getFullYear();
+let intervalId;
+
+function convertion(val) {
+    return (val - 273).toFixed(2);
+}
+
+function currentTime() {
+    let current = new Date();
+    time.innerHTML = current.toLocaleTimeString("en-GB");
+}
+
+btn.addEventListener("click", () => {
+    display.classList.add("active");
+    fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+            inputval.value +
+            "&appid=" +
+            apik
+    )
+        .then((res) => res.json())
+        .then((data) => {
+            let nameval = data["name"];
+            let descrip = data["weather"]["0"]["description"];
+            let tempature = data["main"]["temp"];
+            let wndspd = data["wind"]["speed"];
+
+            city.innerHTML = `Weather of <span>${nameval}<span>`;
+            temp.innerHTML = `Temperature: <span>${convertion(
+                tempature
+            )} C</span>`;
+            description.innerHTML = `Sky Conditions: <span>${descrip}<span>`;
+            wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`;
+        })
+        .catch((err) => {
+            city.innerHTML = "Something went wrong";
+            temp.innerHTML = "";
+            description.innerHTML = "";
+            wind.innerHTML = "";
+        });
+});
+
+inputval.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13) {
+        display.classList.add("active");
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+                inputval.value +
+                "&appid=" +
+                apik
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                let nameval = data["name"];
+                let descrip = data["weather"]["0"]["description"];
+                let tempature = data["main"]["temp"];
+                let wndspd = data["wind"]["speed"];
+
+                city.innerHTML = `Weather of <span>${nameval}<span>`;
+                temp.innerHTML = `Temperature: <span>${convertion(
+                    tempature
+                )} C</span>`;
+                description.innerHTML = `Sky Conditions: <span>${descrip}<span>`;
+                wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`;
+            })
+            .catch((err) => {
+                city.innerHTML = "Something went wrong";
+                temp.innerHTML = "";
+                description.innerHTML = "";
+                wind.innerHTML = "";
+            });
+    }
+});
+intervalId = setInterval(currentTime, 1000);
